1d9c21137343f0d13c68f271dad75649b0d5db35
Andrew Ngo <andrew@Andrews-MacBook-Pro-3.local>
2018-10-11 11:53:19 -0700

    Includes check out server

---
+++ database/indexPostgreSQL.js
@@ -0,0 +1,61 @@
+const pg = require('pg');
+let client = {
+  host: 'ec2-18-144-17-138.us-west-1.compute.amazonaws.com',
+  post: 5432,
+  user: 'power_user',
+  password: '$password',
+  database: 'hackazon'
+}
+const connection = new pg.Client(client);
+
+connection.connect((error, results) => {
+  if (error) {
+    console.log('ERROR failed to connect to DB', error)
+  } else {
+    console.log('Successfully connected to DB!')
+  }
+})
+
+let searchQuery = (productID, callback) => {
+  connection.query(`SELECT * FROM product_table INNER JOIN protection_table ON product_table.product_id = protection_table.product_id WHERE product_table.product_id = ${productID};`, (error, results) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, results.rows[0]);
+    }
+  })
+}
+
+let insertQuery = (product, callback) => {
+  connection.query(`INSERT INTO product_table (name, image, link, shares, price, is_prime, in_stock, giftwrap_available, quantity_max, seller) VALUES ('${product.name}', '${product.image}', '${product.link}', ${product.shares}, ${product.price}, ${product.is_prime}, ${product.in_stock}, ${product.giftwrap_available}, ${product.quantity_max}, '${product.seller}')`, (error, results) => {
+    if (error) {
+      console.log(`ERROR insert query`, error);
+    } else {
+      console.log(`Successfully inserted into DB!`);
+    }
+  });
+
+  connection.query(`INSERT INTO protection_table (available, protection_name, protection_price, years, provider, rating, description) VALUES (${product.available}, '${product.protection_name}', ${product.protection_price}, ${product.years}, '${product.provider}', ${product.rating}, '${product.description}')`, (error, results) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, results);
+    }
+  });
+}
+
+  connection.query(`DELETE FROM product_table WHERE product_id = ${productID}`, (error, results) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, results);
+    }
+  })
+}
+
+module.exports = {
+  searchQuery,
+  insertQuery,
+  updateQuery,
+  deleteQuery,
+}
---
+++ server/indexPostgreSQL.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const redis = require("redis");
+
+const app = express();
+
+const bodyParser = require('body-parser');
+const path = require('path');
+const controller = require('../database/indexPostgreSQL.js');
+
+let client = redis.createClient();
+
+client.on('connect', function(error) {
+  if (error) {
+    console.log('ERROR connecting to redis')
+  } else {
+    console.log('Connected to redis...')
+  }
+});
+
+app.use(bodyParser.json());
+
+app.get('/checkout/:id', (req, res) => {
+
+  client.get(req.params.id, (error, results) => {
+
+    if (results !== null) {
+      res.send(JSON.parse(results))
+    } else {
+      controller.searchQuery(req.params.id, (error, results) => {
+        if (error) {
+          console.error('ERROR searchQuery controller failed')
+        } else {
+
+          let result = {
+            giftwrap_available: results.giftwrap_available,
+            image: results.image,
+            in_stock: results.in_stock,
+            is_prime: results.is_prime,
+            link: results.link,
+            name: results.name,
+            price: results.price,
+            product_id: results.product_id,
+            quantity_max: results.quantity_max,
+            seller: results.seller,
+            shares: results.shares,
+            protection_plan: {
+              description: results.description,
+              exists: results.available,
+              name: results.protection_name,
+              price: results.protection_price,
+              provider: results.provider,
+              rating: results.rating,
+              years: results.years,
+            }
+          }
+          client.setex(req.params.id, 1000000, JSON.stringify(result));
+          res.send(result);
+        }
+      })
+    }
+  })
+})
+
+app.post('/add-product', (req, res) => {
+  controller.insertQuery(req.body, (error, results) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.status(201).send('Post successful!')
+    }
+  })
+})
+
+app.listen(3003, () => console.log('Listening on port 3003...'));
