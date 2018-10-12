12550a66ca646ab02e2fb8928d93425618043f15
Andrew Ngo <andrew@Andrews-MacBook-Pro-3.local>
2018-10-12 10:52:40 -0700

    Includes pm2

--- server/indexPostgreSQL.js
+++ server/indexPostgreSQL.js
@@ -65,9 +65,9 @@ app.get('/checkout/:id', (req, res) => {
   })
 })
 
-app.get('/loaderio-115320b901e0f9f6617dd8d8b4c03fdc', (req, res) => {
-  res.send('loaderio-115320b901e0f9f6617dd8d8b4c03fdc')
-})
+// app.get('/loaderio-115320b901e0f9f6617dd8d8b4c03fdc.txt', (req, res) => {
+//   res.send('loaderio-115320b901e0f9f6617dd8d8b4c03fdc')
+// })
 
 app.post('/add-product', (req, res) => {
   controller.insertQuery(req.body, (error, results) => {
